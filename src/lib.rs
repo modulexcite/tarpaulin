977f7e7ac1039f40d451857034cc040c1741e3aa
xd009642 <danielmckenna93@gmail.com>
2020-03-04 22:54:19 +0000

    Update to latest tarpaulin on master

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,7 @@ use crate::source_analysis::LineAnalysis;
 use crate::statemachine::*;
 use crate::test_loader::*;
 use crate::traces::*;
-use cargo::core::{
-    compiler::{CompileMode, ProfileKind},
-    Package, Shell, Workspace,
-};
+use cargo::core::{compiler::CompileMode, Package, Shell, Workspace};
 use cargo::ops;
 use cargo::ops::{
     clean, compile, CleanOptions, CompileFilter, CompileOptions, FilterRule, LibRule, Packages,
@@ -102,18 +99,19 @@ pub fn launch_tarpaulin(config: &Config) -> Result<(TraceMap, i32), RunError> {
         }
     };
     let mut cargo_config = CargoConfig::new(Shell::new(), cwd, home);
-    let flag_quiet = if config.verbose { None } else { Some(true) };
+    let flag_quiet = !config.verbose;
 
     // This shouldn't fail so no checking the error.
     let _ = cargo_config.configure(
         0u32,
         flag_quiet,
-        &None,
+        None,
         config.frozen,
         config.locked,
         config.offline,
         &config.target_dir,
         &config.unstable_features,
+        &[],
     );
 
     let workspace = Workspace::new(config.manifest.as_path(), &cargo_config)
@@ -131,7 +129,7 @@ pub fn launch_tarpaulin(config: &Config) -> Result<(TraceMap, i32), RunError> {
             spec: vec![],
             target: None,
             profile_specified: config.force_clean,
-            profile_kind: ProfileKind::Dev,
+            requested_profile: "dev".into(),
             doc: false,
         };
         let _ = clean(&workspace, &clean_opt);
@@ -319,9 +317,9 @@ fn get_compile_options<'a>(
         copt.features = config.features.clone();
         copt.all_features = config.all_features;
         copt.no_default_features = config.no_default_features;
-        copt.build_config.profile_kind = match config.release {
-            true => ProfileKind::Release,
-            false => ProfileKind::Dev,
+        copt.build_config.requested_profile = match config.release {
+            true => "release".into(),
+            false => "dev".into(),
         };
         copt.spec =
             match Packages::from_flags(config.all, config.exclude.clone(), config.packages.clone())
